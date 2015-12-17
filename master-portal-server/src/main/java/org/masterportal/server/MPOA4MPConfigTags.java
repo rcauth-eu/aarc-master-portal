bfc7c29b7d52c75ca260e83026936079a7595a58
ttomttom <tamasb@nikhef.nl>
2015-12-17 20:05:11 +0100

    deleted duplicate project

--- master-portal-server/src/main/java/org/masterportal/server/MPOA4MPConfigTags.java
+++
@@ -1,9 +0,0 @@
-package org.masterportal.server;
-
-import edu.uiuc.ncsa.myproxy.oa4mp.server.OA4MPConfigTags;
-
-public interface MPOA4MPConfigTags extends OA4MPConfigTags {
-
-	public static final String MYPROXY_PASSWORD = "password";
-	
-}
