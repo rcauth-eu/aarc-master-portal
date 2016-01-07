e03aebc57885fa2aa3f251b94c26d896dc71bb9b
ttomttom <tamasb@nikhef.nl>
2016-01-07 15:29:49 +0100

    restructure
    delete both myproxy-fork and ncsa-security-all-fork since
    they have been moved into their own separate repository

--- myproxy-fork/myproxy/myproxy-logon/src/main/java/edu/uiuc/ncsa/myproxy/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * MyProxy Logon.
- * <p>
- * This package provides a tool for communicating with a MyProxy instance, a GUI (runnable from the command line)
- * and configuration classes for servers.
- *
- */
-
-package edu.uiuc.ncsa.myproxy;
