5b62ecdc39d475e46b32710993539e71e3434061
Mischa Salle <msalle@nikhef.nl>
2017-02-20 16:17:56 +0100

    Remove unused imports, fix tabsize

--- master-portal/master-portal-server/src/main/java/org/masterportal/oauth2/server/MPForwardingScopeHandler.java
+++ master-portal/master-portal-server/src/main/java/org/masterportal/oauth2/server/MPForwardingScopeHandler.java
@@ -3,10 +3,9 @@ package org.masterportal.oauth2.server;
 import java.util.Map;
 
 import edu.uiuc.ncsa.myproxy.oa4mp.oauth2.servlet.BasicScopeHandler;
-import edu.uiuc.ncsa.security.core.exceptions.GeneralException;
+//import edu.uiuc.ncsa.security.core.exceptions.GeneralException;
 import edu.uiuc.ncsa.security.delegation.server.ServiceTransaction;
 import edu.uiuc.ncsa.security.oauth_2_0.UserInfo;
-import edu.uiuc.ncsa.security.oauth_2_0.server.UnsupportedScopeException;
 
 /**
  * Custom ScopeHandler that adds claims into UserInfo and IDToken 
@@ -14,7 +13,6 @@ import edu.uiuc.ncsa.security.oauth_2_0.server.UnsupportedScopeException;
  * @author "Tamás Balogh"
  */
 public class MPForwardingScopeHandler extends BasicScopeHandler {
-
 
     /**
      * Returns the {@link UserInfo} object passed to it completed with the claims saved by the 
@@ -24,29 +22,28 @@ public class MPForwardingScopeHandler extends BasicScopeHandler {
      * @param transaction the current transaction record
      * @return The UserInfo completed with claims 
      */
-	@Override
-	public UserInfo process(UserInfo userInfo, ServiceTransaction transaction) {
+    @Override
+    public UserInfo process(UserInfo userInfo, ServiceTransaction transaction) {
 
-		//add the claims build based on the requested scopes into the userinfo reply
-		
-		//get claims previously extracted from the transaction
-		MPOA2ServiceTransaction t = (MPOA2ServiceTransaction) transaction;
-		Map<String,Object> claims =  t.getClaims();
-		
-		if ( claims != null ) {
-			//some claims might already by set by now, so instead of overwriting them, 
-			//simple append the claims from the transaction.
-			Map<String,Object> userinfoMap = userInfo.getMap();
-			for ( String claim : claims.keySet()) {
-				userinfoMap.put(claim, claims.get(claim));
-			}
-			userInfo.setMap(userinfoMap);
-		} else {
-			//TODO: fail or just simply pass through? No claims is not necessarily an error 
-			//throw new GeneralException("There were no claims saved for this transaction!");
-		}
-		
-		return userInfo;
+	//add the claims build based on the requested scopes into the userinfo reply
+	
+	//get claims previously extracted from the transaction
+	MPOA2ServiceTransaction t = (MPOA2ServiceTransaction) transaction;
+	Map<String,Object> claims =  t.getClaims();
+	
+	if ( claims != null ) {
+	    //some claims might already by set by now, so instead of overwriting them, 
+	    //simple append the claims from the transaction.
+	    Map<String,Object> userinfoMap = userInfo.getMap();
+	    for ( String claim : claims.keySet()) {
+		userinfoMap.put(claim, claims.get(claim));
+	    }
+	    userInfo.setMap(userinfoMap);
+	} else {
+	    //TODO: fail or just simply pass through? No claims is not necessarily an error 
+	    //throw new GeneralException("There were no claims saved for this transaction!");
 	}
 	
+	return userInfo;
+    }
 }
