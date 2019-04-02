ee600bb3ea0b381934a53c34a182cf31463b1930
Mischa Salle <msalle@nikhef.nl>
2019-04-02 18:49:30 +0200

    Cleanup and clarify MPForwardingClaimsSourceImpl
    The MPForwardingClaimsSourceImpl is currently not doing anything.
    Put lots of clarifying text in the file and remove the overridden methods we
    shouldn't override since they aren't (shouldn't be) used in any case.
    We should ideally add a getClaims() that gets the supported claims from the DS
    but that's non-trivial as it involves the mp-client.

--- master-portal/master-portal-server/src/main/java/eu/rcauth/masterportal/server/MPForwardingClaimsSourceImpl.java
+++ master-portal/master-portal-server/src/main/java/eu/rcauth/masterportal/server/MPForwardingClaimsSourceImpl.java
@@ -1,31 +1,46 @@
 package eu.rcauth.masterportal.server;
 
-import java.util.Set;
+import java.util.Collection;
+import java.util.HashSet;
+
 import edu.uiuc.ncsa.myproxy.oa4mp.oauth2.claims.BasicClaimsSourceImpl;
-import edu.uiuc.ncsa.security.delegation.server.ServiceTransaction;
-import edu.uiuc.ncsa.security.oauth_2_0.server.UnsupportedScopeException;
 import edu.uiuc.ncsa.security.oauth_2_0.server.claims.ClaimSourceConfiguration;
-import net.sf.json.JSONObject;
+
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /* Next imports are to ease the javadoc */
-import eu.rcauth.masterportal.server.servlet.MPOA2AuthorizationServer;
+import net.sf.json.JSONObject;
 import edu.uiuc.ncsa.security.servlet.PresentableState;
-import edu.uiuc.ncsa.myproxy.oa4mp.oauth2.OA2ServiceTransaction;
 import edu.uiuc.ncsa.security.delegation.server.ServiceTransaction;
+import edu.uiuc.ncsa.security.oauth_2_0.server.config.LDAPConfiguration;
+import edu.uiuc.ncsa.myproxy.oa4mp.oauth2.servlet.OA2DiscoveryServlet;
+import edu.uiuc.ncsa.myproxy.oa4mp.oauth2.OA2ServiceTransaction;
 import edu.uiuc.ncsa.myproxy.oa4mp.oauth2.claims.OA2ClaimsUtil;
+import eu.rcauth.masterportal.server.servlet.MPOA2AuthorizationServer;
 
 /**
  * Custom ClaimsSourceImpl that adds claims into UserInfo and IDToken.
+ * We probably want to override {@link #getClaims()} in order to provide
+ * a proper list of claims in the UserInfo. It is called in
+ * {@link OA2DiscoveryServlet#setValues(HttpServletRequest, JSONObject)}.
  * <br>
- * TODO we need to decide whether we want to allow additional handlers, which
- * then also need additional configuration via a {@link ClaimSourceConfiguration}.
  * Currently, all the extra claims from the DS are already forwarded and put in
  * the transaction in
  * {@link eu.rcauth.masterportal.server.servlet.MPOA2AuthorizationServer#prepare(PresentableState)}
  * by
  * {@link OA2ServiceTransaction#setClaims(JSONObject)}.
+ * <br>
+ * If we would want to do additional fancy stuff with the claims (e.g. add
+ * extra local ones) we would need to:
+ * <ul><li>override {@link #process(JSONObject, ServiceTransaction)} and
+ * {@link #process(JSONObject, HttpServletRequest, ServiceTransaction)}
+ * <li>need additional configuration via a {@link ClaimSourceConfiguration},
+ * for details see the constructor {@link MPForwardingClaimsSourceImpl()}
+ * <li>override {@link #isRunAtAuthorization()} to return true, in order not to
+ * be skipped in {@link OA2ClaimsUtil#createBasicClaims}
+ * </ul>
  * <br>
  * NOTE: this ClaimsSourceImpl is called 'on the way' back, when returning from
  * the mp-server to the (e.g. vo-portal) client. It is called by
@@ -39,68 +54,38 @@ import edu.uiuc.ncsa.myproxy.oa4mp.oauth2.claims.OA2ClaimsUtil;
  * also the constructor).
  * 
  * @author "Tamás Balogh"
+ * @author "Mischa Sall&eacute;"
  */
 public class MPForwardingClaimsSourceImpl extends BasicClaimsSourceImpl {
 
     /**
      * Constructor for a MPForwardingClaimsSourceImpl.
-     * It needs to have a {@link ClaimSourceConfiguration} which is enabled.
-     * See for example {@linkplain edu.uiuc.ncsa.security.oauth_2_0.server.config.LDAPConfiguration}
+     * It needs to have a {@link ClaimSourceConfiguration} which is set to enabled.
+     * See for example {@linkplain LDAPConfiguration}
      */
     public MPForwardingClaimsSourceImpl() {
         /* We currently don't need this ClaimsSourceImpl. */
         this.setConfiguration(null);
-/*        ClaimSourceConfiguration claimSourceConfiguration = new ClaimSourceConfiguration();
+/*      ClaimSourceConfiguration claimSourceConfiguration = new ClaimSourceConfiguration();
         claimSourceConfiguration.setEnabled(true);
         this.setConfiguration(claimSourceConfiguration);*/
     }
 
-    @Override
-    // TODO Unclear what this does but it needs to be true or it's skipped in
-    // edu.uiuc.ncsa.myproxy.oa4mp.oauth2.claims.OA2ClaimsUtil#createBasicClaims
-    public boolean isRunAtAuthorization(){
-	/* We currently don't use this ClaimsSourceImpl, so set to false. Also
-	 * need to have a enabled non-null configuration in order to run */
-        return false;
-/*        return true;*/
-    }
-
     /**
-     * Returns the {@link JSONObject} object passed to it completed with the claims saved by the
-     * corresponding transaction.
-     *
-     * @param claims being returned to the client
-     * @param transaction the current transaction record
-     * @return The UserInfo completed with claims 
+     * getClaims produces the set of supported claims returned in the .well-known discovery
+     * @see BasicClaimsSourceImpl#getClaims()
+     * @return a HashSet containing the supported claims.
      */
-    @Override
-    public JSONObject process(JSONObject claims, ServiceTransaction transaction) {
-
-        //add the claims build based on the requested scopes into the userinfo reply
-
-        //get claims previously extracted from the transaction
-        MPOA2ServiceTransaction t = (MPOA2ServiceTransaction) transaction;
-        JSONObject transClaims =  t.getClaims();
-
-        // TODO check logic
-        if ( transClaims != null ) {
-            //some claims might already by set by now, so instead of overwriting them,
-            //simple append the claims from the transaction.
-            for ( String claimKey : (Set<String>)transClaims.keySet()) {
-                claims.put(claimKey, transClaims.get(claimKey));
-            }
-        } else {
-            //TODO fail or just simply pass through? No claims is not necessarily an error
-            //throw new GeneralException("There were no claims saved for this transaction!");
-        }
-
+/*  @Override
+    // TODO implement a getClaims that gets the list from the DS's .well-known
+    // endpoint but that's only known to the client. Note that it's also not
+    // strictly necessary, see claims_supported on
+    // https://openid.net/specs/openid-connect-discovery-1_0.html#ProviderMetadata
+    //  "Note that for privacy or other reasons, this might not be an
+    //	 exhaustive list." 
+    public Collection<String> getClaims() {
+        HashSet<String> claims = (HashSet<String>)super.getClaims();
+        claims.add("some_non_basic_supported_claim");
         return claims;
-    }
-
-    @Override
-    // TODO check that this makes any sense and is needed
-    public JSONObject process(JSONObject claims, HttpServletRequest request, ServiceTransaction transaction) throws UnsupportedScopeException {
-//        super.process(claims,request,transaction);
-        return process(claims, transaction);
-    }
+    }*/
 }
